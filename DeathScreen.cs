3f558bd60025955e46595720a408553221a5686f
ark020 <146484150+ark020@users.noreply.github.com>
2023-10-22 23:43:25 -0500

    This is a blank death screen for CoNiya

---
+++ DeathScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class DeathScreen : Form
+    {
+        public DeathScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void DeathScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
